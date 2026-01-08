b2c89d2741ce8b1e59cda338ebe73273a436b34a
Maxim Kurbatov <max@cubly.ru>
2026-01-08 22:17:18 +0500

    Update patches

--- packages/compile.sh
+++ packages/compile.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-set -e
+set -e -x
+
+# Copy configuration files
+cp /home/me/openwrt/router.config /home/me/openwrt/.config
+cat /home/me/openwrt/owrt-packages/packages.config >> /home/me/openwrt/.config
+cat /home/me/openwrt/owrt-packages/packages.feed >> /home/me/openwrt/feeds.conf.default
+
+# Update feeds
+./scripts/feeds update custom
+./scripts/feeds install -a -p custom
+
+# Update config based on config diffs
+make defconfig
 
 # Add other package compilation instructions below
 make -j$(nproc) V=s package/cups/compile
